0f898a22a74dd6bab5b7155033b7a6c869b2b2db
Đặng Trần Hoàng Long <dangtranhoanglong2411@gmail.com>
2025-06-26 08:29:26 +0700

    a

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -2,7 +2,14 @@
    Motor driver function definitions - by James Nugen
    *************************************************************/
 
-#ifdef L298_MOTOR_DRIVER
+#ifdef BLD300B_MOTOR_DRIVER
+  #define LEFT_DIR_PIN    5
+  #define LEFT_PWM_PIN    6
+  #define RIGHT_DIR_PIN   9
+  #define RIGHT_PWM_PIN   10
+  #define MOTOR_ENABLE_PIN 4
+
+#elif define L298_MOTOR_DRIVER
   #define RIGHT_MOTOR_BACKWARD 5
   #define LEFT_MOTOR_BACKWARD  6
   #define RIGHT_MOTOR_FORWARD  9
